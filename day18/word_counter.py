d9bcb6bf19bf5e0fc4772a97d148c06d5240d2c2
DANR1BEIRO <danribeiro001@gmail.com>
2025-01-17 13:38:44 -0300

    Word counter

--- day18/word_counter.py
+++ day18/word_counter.py
@@ -1,19 +1,25 @@
 def get_word(prompt):
     """
-    Count the number of words in a given string
+    Prompt the user for a non empty string input
     
     Parameters:
-    Input string (str): The string whose words need to be counted
+    Prompt (str): The message displayed to the user to ask for input
     
     returns:
-    int: The number of words in the input string """
+    str: The non empty string entered by the user"""
     while True:
         text = input(prompt)
-        if text.strip():
+        if text.strip(): # ensure that the input isn't empty or just whitespace
             return text
         print("can't be a empty text")
 
 def word_counter():
+    """
+    Counts the number of words in the given string
+    
+    returns:
+    int: The number of words in the input
+    """
     text = get_word("Enter a text: ")
     count = text.split()
     return len(count)
